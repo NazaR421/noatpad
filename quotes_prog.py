bb619012d5af861c30b3d64addf04feef146c787
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2023-11-27 13:12:51 +0200

    Update quotes_prog.py

--- quotes_prog.py
+++ quotes_prog.py
@@ -12,3 +12,22 @@ with open('quotes.txt','a',encoding='UTF-8') as file:
 with open('quotes.txt','r',encoding='UTF-8') as file:
     data=file.read()
     print(data)
+
+a= input("Хочете ще додати цитати і авторів?(так,ні)")
+
+while a=="так":
+    b=input ("Введіть цитату:")
+
+    with open('quotes.txt','a',encoding='UTF-8') as file:
+        file.write(f'\n{b}\n')
+
+    with open('quotes.txt','r',encoding='UTF-8') as file:
+        data=file.read()
+        print(data)
+    a= input("Хочете ще додати цитати і авторів?(так,ні)")
+
+else:
+    with open('quotes.txt','r',encoding='UTF-8') as file:
+        data=file.read()
+        print(data)
+    
